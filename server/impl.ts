df0e2fde483327b9e656d7af30a6097ea6c04527
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-04-15 17:37:13 +0200

    Format and regenerate server

--- server/impl.ts
+++ server/impl.ts
@@ -1,31 +1,41 @@
-import { Methods, Context } from "./.hathora/methods";
-import { Response } from "../api/base";
+import { Methods, Context } from './.hathora/methods'
+import { Response } from '../api/base'
 import {
-  Location,
-  Player,
-  GameState,
-  UserId,
-  IInitializeRequest,
-  IJoinGameRequest,
-  IMoveToRequest,
-} from "../api/types";
+	Location,
+	Player,
+	GameState,
+	UserId,
+	IInitializeRequest,
+	IJoinGameRequest,
+	IMoveToRequest,
+} from '../api/types'
 
-type InternalState = GameState;
+type InternalState = GameState
 
 export class Impl implements Methods<InternalState> {
-  initialize(ctx: Context, request: IInitializeRequest): InternalState {
-    return {
-      players: [],
-    };
-  }
-  joinGame(state: InternalState, userId: UserId, ctx: Context, request: IJoinGameRequest): Response {
-    return Response.error("Not implemented");
-  }
-  moveTo(state: InternalState, userId: UserId, ctx: Context, request: IMoveToRequest): Response {
-    return Response.error("Not implemented");
-  }
-  getUserState(state: InternalState, userId: UserId): GameState {
-    return state;
-  }
-  onTick(state: InternalState, ctx: Context, timeDelta: number): void {}
+	initialize(ctx: Context, request: IInitializeRequest): InternalState {
+		return {
+			players: [],
+		}
+	}
+	joinGame(
+		state: InternalState,
+		userId: UserId,
+		ctx: Context,
+		request: IJoinGameRequest,
+	): Response {
+		return Response.error('Not implemented')
+	}
+	moveTo(
+		state: InternalState,
+		userId: UserId,
+		ctx: Context,
+		request: IMoveToRequest,
+	): Response {
+		return Response.error('Not implemented')
+	}
+	getUserState(state: InternalState, userId: UserId): GameState {
+		return state
+	}
+	onTick(state: InternalState, ctx: Context, timeDelta: number): void {}
 }
